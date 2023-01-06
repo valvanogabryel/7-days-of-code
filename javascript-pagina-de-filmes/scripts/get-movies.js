87afd52a0d3687f32abf7c9983b6bafdc9f7017e
valvanogabryel <valvanogabryel@gmail.com>
2023-01-06 10:36:15 -0300

    Update get-movies.js

--- javascript-pagina-de-filmes/scripts/get-movies.js
+++ javascript-pagina-de-filmes/scripts/get-movies.js
@@ -1,6 +1,8 @@
 async function getMovies() {
-    const url = '';
+    const url = 'https://api.themoviedb.org/3/movie/550?api_key=751799e0dc883576b6b755416df283ab';
     const response = await fetch(url);
     const movies = await response.json();
     return movies
 }
+
+console.log(getMovies());
