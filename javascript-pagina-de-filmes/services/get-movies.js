b4a3ea7080d87572477c43605bc2561b1ed2558b
valvanogabryel <valvanogabryel@gmail.com>
2023-01-10 13:23:59 -0300

    ;

--- javascript-pagina-de-filmes/services/get-movies.js
+++ javascript-pagina-de-filmes/services/get-movies.js
@@ -1,5 +1,4 @@
 import { API_key } from './environment/key.js';
-import API_key from './environment/key.js';
 
 async function getPopularMovies() {
     const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_key}&language=pt-BR&page=1`;
